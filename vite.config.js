fc5d07bac1593499d65a497b88a95e9e52728e6a
magarikado <ka5ryouma@gmail.com>
2026-03-01 22:49:35 +0900

    pwa対応

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,47 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Artport',
+        short_name: 'Artport',
+        description: 'アート発見プラットフォーム',
+        theme_color: '#f7f4ef',
+        background_color: '#f7f4ef',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/firebasestorage\.googleapis\.com\//,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'firebase-storage',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60 // 30日
+              }
+            }
+          }
+        ]
+      }
+    })
+  ]
+})
